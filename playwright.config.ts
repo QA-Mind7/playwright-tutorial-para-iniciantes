dfde783d737ec3e045683b6818ddbc565e75028b
Thairam <thayram01@gmail.com>
2025-10-27 22:28:58 -0300

    define reporter=blob para ambiente de CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : Config.WORKERS,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -33,6 +33,8 @@ export default defineConfig({
     trace: 'on-first-retry',
     baseURL: Config.BASE_URL,
     testIdAttribute: 'data-test',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
